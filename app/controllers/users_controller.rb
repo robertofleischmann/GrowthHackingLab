dc563d068adf59b8e7e84840a85ac4e24e71df6f
Roberto Fleischmann <rf.fresh@gmail.com>
2013-12-27 18:58:31 -0300

    Ruby on Rails OneMonth Course Finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
-  	#@hacks = Hack.where(:user_id => @user.id)
+  	# @hacks = Hack.where(:user_id => @user.id)
   	@hacks = @user.hacks
   end
 end
